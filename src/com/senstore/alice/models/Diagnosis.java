5893cefd475ebaa169b8812ece8fde3039832e21
Muniu Kariuki <muniu@bityarn.co.ke>
2012-05-18 17:16:24 +0300

    Diagnosis and Log models

---
+++ src/com/senstore/alice/models/Diagnosis.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.senstore.alice.models;
+
+/**
+ * @author Muniu Kariuki - muniu@bityarn.co.ke
+ * 
+ */
+public class Diagnosis {
+
+	/**
+	 * 
+	 {"chat_length":2,"created_at":"2012-05-18T12:34:44Z","guide":null,"id":
+	 * 237,"input":"Option One","option1":null,"option2":null,"option3":null,
+	 * "option4":null,"option5":null,"reply":
+	 * "This is an emergency, please go to the nearest ER shown below"
+	 * ,"response_type"
+	 * :"3","secret_hash":null,"select_type":"voice","updated_at"
+	 * :"2012-05-18T12:34:44Z","user_id":"1234"}
+	 */
+	public Diagnosis() {
+
+	}
+
+}
---
+++ src/com/senstore/alice/models/Log.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.senstore.alice.models;
+
+/**
+ * @author Muniu Kariuki - muniu@bityarn.co.ke
+ * 
+ */
+public class Log {
+
+	/**
+	 * <record> <id type="integer">8</id> <user-id>1234</user-id>
+	 * <log-type>3</log-type> <location>california</location> <secret-hash
+	 * nil="true"/> <created-at
+	 * type="datetime">2012-05-18T14:13:48Z</created-at> <updated-at
+	 * type="datetime">2012-05-18T14:13:48Z</updated-at> </record>
+	 */
+	public Log() {
+
+	}
+
+}
