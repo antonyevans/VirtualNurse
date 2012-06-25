c40030105e35aeb044d1b5b29cfd17a6e82f26fe
Geoffrey Mimano <gmimano@bityarn.co.ke>
2012-06-25 18:08:19 +0300

    Adding splash screen to application

---
+++ src/com/senstore/alice/activities/SplashActivity.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.senstore.alice.activities;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.senstore.alice.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author Mimano Muthondu gmimano@bityarn.co.ke
+ *
+ */
+public class SplashActivity extends Activity {
+	
+	private long splashDelay = 3000; //3 seconds
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		// Set Full Screen Since we have a Tittle Bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				
+		
+		//add layout for the splash screen
+		setContentView(R.layout.splash_screen);
+		
+		TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+				finish();
+				Intent mainIntent = new Intent(SplashActivity.this, Alice.class);
+				startActivity(mainIntent);
+				
+				
+			}
+		};
+		
+		Timer timer = new Timer();
+		
+		timer.schedule(task, this.splashDelay);
+		
+		
+	}
+
+}
