54c3489acd7a28deaa9a2482876c5b8e1692f7a6
Antony Evans <antonyevans@gmail.com>
2012-12-13 16:59:16 -0800

    created body list

--- src/com/senstore/alice/menus/BodyGuide.java
+++ src/com/senstore/alice/menus/BodyGuide.java
@@ -9,27 +9,111 @@ public enum BodyGuide {
 
 		@Override
 		public String userFriendlyName() {
-			return "Head";
+			return "Head & Neck";
 		}
 
 
 		@Override
 		public String result() {
-			return "Head";
+			return "Head and neck";
 		}
 
 	},
-	SHOULDERS {
+	TORSO {
 
 		@Override
 		public String userFriendlyName() {
-			return "Shoulders";
+			return "Torso";
 		}
 
 
 		@Override
 		public String result() {
-			return "Shoulders";
+			return "Torso";
+		}
+
+	},
+	LIMBS {
+
+		@Override
+		public String userFriendlyName() {
+			return "Limbs (arms and legs)";
+		}
+
+
+		@Override
+		public String result() {
+			return "Limbs (arms and legs)";
+		}
+
+	},
+	SEX_WOMEN {
+
+		@Override
+		public String userFriendlyName() {
+			return "Sex organs (women)";
+		}
+
+
+		@Override
+		public String result() {
+			return "Sex organs (women)";
+		}
+
+	},
+	SEX_MEN {
+
+		@Override
+		public String userFriendlyName() {
+			return "Sex organs (men)";
+		}
+
+
+		@Override
+		public String result() {
+			return "Sex organs (men)";
+		}
+
+	},
+	MENTAL {
+
+		@Override
+		public String userFriendlyName() {
+			return "Mental / emotional";
+		}
+
+
+		@Override
+		public String result() {
+			return "Mental / emotional";
+		}
+
+	},
+	GENERAL_ADULT {
+
+		@Override
+		public String userFriendlyName() {
+			return "General (adults)";
+		}
+
+
+		@Override
+		public String result() {
+			return "General (adults)";
+		}
+
+	},
+	GENERAL_CHILD {
+
+		@Override
+		public String userFriendlyName() {
+			return "General (children)";
+		}
+
+
+		@Override
+		public String result() {
+			return "General (children)";
 		}
 
 	};
